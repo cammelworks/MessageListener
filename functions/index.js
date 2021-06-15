d91e3846def9668d74db960ebe1362dca53915ac
武田拓樹 <HirokiTakeda@meishoumisetteimainasuno-MacBook-Air.local>
2021-06-15 20:11:42 +0900

    Cloud FireStoreに新しくデータが追加されたときに受信するメンバーのトークンを取得する

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,53 @@
 const functions = require("firebase-functions");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
+var fireStore = admin.firestore()
+
+
+exports.createUser = functions.firestore
+  .document('teams/{teamName}/chats/{chatID}')
+  .onCreate((snap, context) => {
+    const newValue = snap.data();
+
+    const teamName = context.params.teamName
+    const message = newValue.message
+    const sender = newValue.sender
+
+    functions.logger.info(teamName, { structuredData: true });
+    functions.logger.info(message, { structuredData: true });
+    functions.logger.info(sender, { structuredData: true });
+
+
+    var teamsRef = fireStore.collection('teams');
+    teamsRef.doc(teamName).collection('users').get()
+      .then(TeamSnapshot => {
+        if (TeamSnapshot.empty) {
+          functions.logger.info('No such collection!', { structuredData: true });
+        } else {
+          TeamSnapshot.forEach(doc => {
+            var usersRef = fireStore.collection('users');
+            usersRef.doc(doc.id).collection('tokens').get()
+              .then(UserSnapshot => {
+                if (UserSnapshot.empty) {
+                  functions.logger.info('No such collection!', { structuredData: true });
+                } else {
+                  UserSnapshot.forEach(doc => {
+                    if (doc.id == sender) {
+                      return;
+                    }
+                    functions.logger.info(doc.id, { structuredData: true });
+                });
+              }
+            })
+            .catch(err => {
+              functions.logger.info('not found', { structuredData: true });
+            })
+          });
+        }
+      })
+      .catch(err => {
+        functions.logger.info('not found', { structuredData: true });
+      })
+  });
